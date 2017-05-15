46085799eddf93178a2fd77dc4244ceaf515f240
RayanO <rayanouzeri@gmail.com>
2017-05-15 22:10:00 +0200

    creating a login activity in order for the users to login

---
+++ app/src/main/java/com/example/xiao/hackathonenac/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.xiao.hackathonenac;
+
+/**
+ * Created by RayanO on 15/05/2017.
+ */
+
+public class LoginActivity {
+}
--- app/src/main/java/com/example/xiao/hackathonenac/MainActivity.java
+++ app/src/main/java/com/example/xiao/hackathonenac/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.xiao.hackathonenac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hypertrack.lib.HyperTrack;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HyperTrack.initialize(this,"pk_51e90839ad5ee373fa2c47e7854f9daa8172c807");
+
+
     }
 }
